bbc62f21d68c890f0bbf38a57f3235973510bc93
Hideaki Masuda <masuda@leapmind.io>
2019-05-11 07:02:50 +0900

    add pipeline files

---
+++ .buildkite/pipeline.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# exit immediately on failure, or if an undefined variable is used
+set -eu
+
+# begin the pipeline.yml file
+echo "steps:"
+
+# add a new command step to run the tests in each test directory
+for test_dir in test/*/; do
+  echo "  - command: \"echo "${test_dir}"\""
+  echo "    label: \"test: "${test_dir}"\""
+done
