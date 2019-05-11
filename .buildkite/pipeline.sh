f9b1985eaaaa14d31723a96d7f2d5732df2e894f
Hideaki Masuda <masuda@leapmind.io>
2019-05-11 09:52:01 +0900

    update pipeline

--- .buildkite/pipeline.sh
+++ .buildkite/pipeline.sh
@@ -7,7 +7,7 @@ set -eu
 echo "steps:"
 
 # add a new command step to run the tests in each test directory
-for test_dir in $(ls -d ./test); do
+for test_dir in $(cd ./test && ls -d test_*); do
   echo "  - command: \"echo "${test_dir}"\""
   echo "    label: \"test: "${test_dir}"\""
 done
